8a929b615691d88aa9988d7d0ea5850b0648d433
Maya Uribe <mariale.uribe@gmail.com>
2025-01-23 16:30:17 +0100

    exercise 4: apply styles

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { range } from '../../utils';
+import { checkGuess } from '../../game-helpers';
 
-function Guess({ value }) {
+function Cell({ letter, status }) {
+  const className = status ? `cell ${status}` : 'cell';
+  return <span className={className}>{letter}</span>;
+}
+
+function Guess({ value, answer }) {
+  const guessStatus = value ? checkGuess(value, answer) : null;
+
   return (
     <p className="guess">
-      {range(5).map((num) => (
-        <span key={num} className="cell">
-          {value ? value[num] : undefined}
-        </span>
-      ))}
+      {range(5).map((num) => {
+        const letter = value ? value[num] : undefined;
+        const status = guessStatus ? guessStatus[num].status : undefined;
+
+        return <Cell key={num} letter={letter} status={status} />;
+      })}
     </p>
   );
 }

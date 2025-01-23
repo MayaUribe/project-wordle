e86e512f3e4439e800462b8886f91b3663032d11
Maya Uribe <mariale.uribe@gmail.com>
2025-01-23 14:44:45 +0100

    exercise 1: add guess word input support

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function GuessInput() {
+  const [guess, setGuess] = React.useState('');
+
+  function formatGuess(guess) {
+    return guess.toUpperCase();
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    setGuess('');
+    console.info({ guess });
+  }
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        required
+        value={guess}
+        minlength={5}
+        maxlength={5}
+        pattern="[A-Z]{5}"
+        title="5 letter word"
+        onChange={(event) => setGuess(formatGuess(event.target.value))}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
